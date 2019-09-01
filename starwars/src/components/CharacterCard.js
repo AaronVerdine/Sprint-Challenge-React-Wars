f7f92ed25a31a757ccfc4bce1c6a95e04b055522
Your name <a.m.verdine@gmail.com>
2019-09-01 19:34:41 -0400

    Add CharacterCard template

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,7 +4,13 @@ function CharacterCard(props) {
 
 
     return (
+        <div>The force will be with you...always
+             <h2>Name of Character</h2>
+                 <h3>Character Gender</h3>
+                    <img src="https://swapi.co/api/planets/1/"/>
         
+        </div>
+       
     )
 }
 
