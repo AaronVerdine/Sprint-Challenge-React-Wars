6d213eca02af4b3cc1857d1e239bf1df68328a50
Your name <a.m.verdine@gmail.com>
2019-09-01 21:40:34 -0400

    Update App.scss

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
+import '
+
 function CharacterCard(props) {
 console.log(props)
 
     return (
-        <Card>
+        <Card className="card-stylz">
              <h2>{props.name}</h2>
                  <h3>{props.gender}</h3>
                     <img src="http://placekitten.com/200/300" alt="random kitties" />
-                   
-
-        
-        <Card>
+        </Card>
        
     )
 }
